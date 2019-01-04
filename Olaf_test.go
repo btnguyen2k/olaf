0298d72d2e25730ea8d634c95600a71d45bc0c24
Thanh Nguyen <btnguyen2k@gmail.com>
2019-01-04 10:27:31 +0700

    More unit tests

--- Olaf_test.go
+++ Olaf_test.go
@@ -1,6 +1,7 @@
 package olaf
 
 import (
+	"math/big"
 	"testing"
 	"time"
 )
@@ -29,45 +30,152 @@ func TestWaitTillNextMillisec(t *testing.T) {
 	}
 }
 
+func TestOlaf_Id64(t *testing.T) {
+	o := NewOlaf(1981)
+	const numItems = 10000000
+	items := [numItems]uint64{}
+	for i := 0; i < numItems; i++ {
+		id := o.Id64()
+		items[i] = id
+		if i > 0 && items[i] <= items[i-1] {
+			t.Errorf("Generated ID is invalid: items[%d]=%d must be less than items[%d]=%d.", i-1, items[i-1], i, items[i])
+		}
+	}
+}
+
+func TestOlaf_Id64Hex(t *testing.T) {
+	o := NewOlaf(1981)
+	const numItems = 10000000
+	items := [numItems]string{}
+	for i := 0; i < numItems; i++ {
+		id := o.Id64Hex()
+		items[i] = id
+		if i > 0 && items[i] <= items[i-1] {
+			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+		}
+	}
+}
+
+func TestOlaf_Id64Ascii(t *testing.T) {
+	o := NewOlaf(1981)
+	const numItems = 10000000
+	items := [numItems]string{}
+	for i := 0; i < numItems; i++ {
+		id := o.Id64Ascii()
+		items[i] = id
+		if i > 0 && items[i] <= items[i-1] {
+			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+		}
+	}
+}
+
+func TestOlaf_Id128(t *testing.T) {
+	o := NewOlaf(1981)
+	const numItems = 10000000
+	items := [numItems]*big.Int{}
+	for i := 0; i < numItems; i++ {
+		id := o.Id128()
+		items[i] = id
+		if i > 0 && items[i].Cmp(items[i-1]) <= 0 {
+			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1].String(), i, items[i].String())
+		}
+	}
+}
+
+func TestOlaf_Id128Hex(t *testing.T) {
+	o := NewOlaf(1981)
+	const numItems = 10000000
+	items := [numItems]string{}
+	for i := 0; i < numItems; i++ {
+		id := o.Id128Hex()
+		items[i] = id
+		if i > 0 && items[i] <= items[i-1] {
+			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+		}
+	}
+}
+
+func TestOlaf_Id128Ascii(t *testing.T) {
+	o := NewOlaf(1981)
+	const numItems = 10000000
+	items := [numItems]string{}
+	for i := 0; i < numItems; i++ {
+		id := o.Id128Ascii()
+		items[i] = id
+		if i > 0 && items[i] <= items[i-1] {
+			t.Errorf("Generated ID is invalid: items[%d]=%s must be less than items[%d]=%s.", i-1, items[i-1], i, items[i])
+		}
+	}
+}
+
 func TestOlaf_ExtractTime64(t *testing.T) {
 	o := NewOlaf(1981)
+	now := time.Now()
 	id64 := o.Id64()
 	time := o.ExtractTime64(id64)
-	t.Log("id [", id64, "] --> time [", time, "]")
+	delta := time.UnixNano() - now.UnixNano()
+	if delta > 1000000 || delta < -1000000 {
+		//delta must be within 1ms
+		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+	}
 }
 
 func TestOlaf_ExtractTime64Hex(t *testing.T) {
 	o := NewOlaf(1981)
+	now := time.Now()
 	id64Hex := o.Id64Hex()
 	time := o.ExtractTime64Hex(id64Hex)
-	t.Log("id [", id64Hex, "] --> time [", time, "]")
+	delta := time.UnixNano() - now.UnixNano()
+	if delta > 1000000 || delta < -1000000 {
+		//delta must be within 1ms
+		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+	}
 }
 
 func TestOlaf_ExtractTime64Ascii(t *testing.T) {
 	o := NewOlaf(1981)
+	now := time.Now()
 	id64Ascii := o.Id64Ascii()
 	time := o.ExtractTime64Ascii(id64Ascii)
-	t.Log("id [", id64Ascii, "] --> time [", time, "]")
+	delta := time.UnixNano() - now.UnixNano()
+	if delta > 1000000 || delta < -1000000 {
+		//delta must be within 1ms
+		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+	}
 }
 
 func TestOlaf_ExtractTime128(t *testing.T) {
 	o := NewOlaf(1981)
+	now := time.Now()
 	id128 := o.Id128()
-	id128Str := id128.String()
 	time := o.ExtractTime128(id128)
-	t.Log("id [", id128Str, "] --> time [", time, "]")
+	delta := time.UnixNano() - now.UnixNano()
+	if delta > 1000000 || delta < -1000000 {
+		//delta must be within 1ms
+		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+	}
 }
 
 func TestOlaf_ExtractTime128Hex(t *testing.T) {
 	o := NewOlaf(1981)
+	now := time.Now()
 	id128Hex := o.Id128Hex()
 	time := o.ExtractTime128Hex(id128Hex)
-	t.Log("id [", id128Hex, "] --> time [", time, "]")
+	delta := time.UnixNano() - now.UnixNano()
+	if delta > 1000000 || delta < -1000000 {
+		//delta must be within 1ms
+		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+	}
 }
 
 func TestOlaf_ExtractTime128Ascii(t *testing.T) {
 	o := NewOlaf(1981)
+	now := time.Now()
 	id128Ascii := o.Id128Ascii()
 	time := o.ExtractTime128Ascii(id128Ascii)
-	t.Log("id [", id128Ascii, "] --> time [", time, "]")
+	delta := time.UnixNano() - now.UnixNano()
+	if delta > 1000000 || delta < -1000000 {
+		//delta must be within 1ms
+		t.Errorf("Invalid extracted time %s (delta nano: %d).", time, delta)
+	}
 }
